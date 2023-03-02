5bc9bf1a44f57057e0e82013d084b7c150866031
Anderson B Silva (Ander) <32654298+oanderoficial@users.noreply.github.com>
2023-03-01 21:25:56 -0300

    Update envio_e-mail.py

--- envio_e-mail.py
+++ envio_e-mail.py
@@ -15,7 +15,7 @@ mensagem.set_content('Olá, venho lembrá-lo de estudar para a prova de estatís
 
 # Envio da mensagem de e-mail
 try:
-    with smtplib.SMTP_SSL('smtp.provedordeemail, 465') as smtp:
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
         smtp.login(email, senha)
         smtp.send_message(mensagem)
         print("E-mail enviado com sucesso!")
